41c5fffd66c40939eae3856f83302b35e3741562
susuaung <susuaung.ms@gmail.com>
2017-04-24 14:00:14 +0900

    Remove package

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Node Boilerplate
+# Timestamp Microservice
+
+## What does it do?
+
+It accepts a string parameter and check whether the string contains either a unix timestamp or a natural language date (e.g. Janugary 1, 2016).
+
+If it does, it returns both the unix timestamp and the natural language form of that date.
+
+If it does not contain a date or unix timestamp, it returns null for those properties.
--- server/server.js
+++ server/server.js
@@ -3,17 +3,12 @@ require('./config/config');
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
-const flash = require('express-flash');
 const compression = require('compression');
 const consolidate = require('consolidate');
 const assert = require('assert');
 const _ = require('lodash');
 const cookieParser = require('cookie-parser')
 
-
-const {passport} = require('./authenticate/passport');
 const {mongoose} = require('./db/mongoose');
 
 const publicPath = path.join(__dirname + './../public');
@@ -33,28 +28,15 @@ app.engine('html', consolidate.ejs);
 app.set('view engine', 'html');
 app.set('views', __dirname + "/views");
 
-app.use(flash());
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-
-app.use(session({
-	secret: process.env.SECRET,
-	cookie: {maxAge: 3600000 },
-	store: new MongoStore({url: process.env.MONGODB_URI}),
-	resave: false,
-	saveUninitialized: false
-}));
-
 app.use(passport.initialize());
 app.use(passport.session());
 
-/* initialize database */
-require('./db/seed');
 
 app.get('/', (req, res) => {
-  res.send('Node Boilerplate');
+  res.send('Timestamp');
 });
 
 app.use((req,res) => {
